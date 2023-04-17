f6599629e3c66fbb8160311d7acc7a5e978b657d
Orest <Orest347@gmail.com>
2023-04-17 13:45:56 +0300

    commit2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,4 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-console.log(hello);
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems.map(({ preview, original, description }) => `
+  <li data-gallery-item="${description}" class="gallery__item">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </li>
+`);
+gallery.insertAdjacentHTML('beforeend', markup.join(''));
+gallery.addEventListener('click', onClick);
+let currentItem;
+function onClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const itemCard = evt.target.parentNode.dataset.galleryItem;
+  currentItem = galleryItems.find(({ description }) => description === itemCard);
+  console.log(currentItem)};
+
+
+
+import SimpleLightbox from "simplelightbox";
