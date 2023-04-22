61553f0e958b0accd22144c7168ae4e3e6551c2e
Orest <Orest347@gmail.com>
2023-04-22 16:53:40 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,27 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery');
-const markup = galleryItems.map(({ preview, original, description }) => `
-  <li data-gallery-item="${description}" class="gallery__item">
-    <img class="gallery__image" src="${preview}" alt="${description}" />
-  </li>
-`);
-gallery.insertAdjacentHTML('beforeend', markup.join(''));
-gallery.addEventListener('click', onClick);
-let currentItem;
-function onClick(evt) {
-  evt.preventDefault();
-  if (!evt.target.classList.contains('gallery__image')) {
-    return;
-  }
-  const itemCard = evt.target.parentNode.dataset.galleryItem;
-  currentItem = galleryItems.find(({ description }) => description === itemCard);
-  console.log(currentItem)};
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`
+  }).join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
 
-
-import SimpleLightbox from "simplelightbox";
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+
+const currentKey = 'feedback-form-state';
+
+form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(currentKey)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInputData(e) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(currentKey, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log({ email: email.value, message: message.value });
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  localStorage.removeItem(currentKey);
+  e.currentTarget.reset();
+  dataForm = {};
+}
