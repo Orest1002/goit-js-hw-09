17b9c1d267bfb655fc2a6fc433be423b0736dfb3
Orest <Orest347@gmail.com>
2023-04-25 21:03:41 +0300

    task3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,7 +3,6 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix, { Notify } from 'notiflix';
 
-
  const btnStart = document.querySelector('[data-start]');
  const timeDays = document.querySelector('[data-days]');
  const timeHours = document.querySelector('[data-hours]');
@@ -26,7 +25,6 @@ import Notiflix, { Notify } from 'notiflix';
         }else{
             btnStart.disabled = true;  Notify.warning('Please choose a date in the future');
         };
-
     },
   };
 
@@ -38,14 +36,13 @@ function convertMs(ms) {
     const hour = minute * 60;
     const day = hour * 24;
   
-   
     const days = Math.floor(ms / day);
     const hours = Math.floor((ms % day) / hour);
     const minutes = Math.floor(((ms % day) % hour) / minute);
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   
     return { days, hours, minutes, seconds };
-  }
+  };
 function addLeadingZero(value) {
     return String(value).padStart(2, 0); 
 };
